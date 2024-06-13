743e7834fd4aced6a7c1f636acbd01ec73326c6d
adeliadeswita <adelswita@gmail.com>
2024-06-13 14:28:39 +0700

    update message gagal histori_aksi
    merubah isi message gagal

--- histori_aksi.php
+++ histori_aksi.php
@@ -15,10 +15,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["selesai"])) {
     $delete_result = mysqli_query($koneksi, $delete_query);
 
     if ($update_result && $delete_result) {
-        header("Location: histori_admin.php?message=Pengajuan dengan NIM/NIP <b>$username</b> sudah berhasil diverifikasi");
+        header("Location: histori_admin.php?message=Pengajuan dengan NIM/NIP <b>$username</b> sudah berhasil diambil.");
     } else {
         $error_message = mysqli_error($koneksi);
-        header("Location: histori_admin.php?message=Gagal: " . $error_message);
+        header("Location: histori_admin.php?message=Pengajuan gagal diambil: " . $error_message);
     }
 }
 
