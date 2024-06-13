7aeae79aec7c0a4c65d825f71933e927bcc12f7b
zhafirah aqilah <zhafirahaqilahh@gmail.com>
2024-06-13 13:58:56 +0700

    update title
    ubah jadi sesuai page

--- ubah_barang.php
+++ ubah_barang.php
@@ -2,7 +2,7 @@
 <html lang="id"> 
 <head> 
     <meta charset="UTF-8"> 
-    <title>ganti sesuai page - CariSini UPNVJ</title> 
+    <title>Informasi Temuan - CariSini UPNVJ</title> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;700&display=swap" rel="stylesheet">
 
