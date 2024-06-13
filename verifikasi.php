50bd872eff5de631c39a0531084941c71396b755
adeliadeswita <adelswita@gmail.com>
2024-06-13 17:20:18 +0700

    update verisikasi.php
    mindahin styling menjadi ke file eksternal

--- verifikasi.php
+++ verifikasi.php
@@ -1,4 +1,5 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,96 +8,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-..." crossorigin="anonymous">
-  
-    <style>
-        .custom-th {
-            color: #186F65;
-            font-size: smaller;
-            text-align:center;
-        }
-
-        .title {
-            font-size:x-large;
-            font-weight:bold;
-        }
-
-        *{
-            margin: 0px;
-            padding: 0px;
-            box-sizing: border-box;
-            outline: none;
-            border: none;
-            text-decoration: none;
-        }
-
-        body{
-            display:block;    
-            font-family: 'Poppins';
-        }
-
-        .logo{
-            font-family:'Poppins';
-            font-size:x-large;
-            font-weight:bold;
-            color:white;
-            padding: 0px 0px 0px 30px;
-        }
-
-        nav{
-            display:flex;
-            font-size:medium;
-            background-color:#186F65;
-            justify-content: space-between;
-            align-items: center;
-            position: fixed;
-            padding: 5px 0px 5px 0px;
-            top:0;
-            left:0;
-            right:0;
-            z-index:9999;
-            text-decoration:none;
-        }
-
-        nav ul{
-            font-weight: bold;
-            list-style-type:none;
-            margin:0;
-            padding: 8px;
-            overflow:hidden;
-        }
-
-        nav ul li{
-            font-weight: bold;
-            float: left;
-        }
-
-        nav ul li a{
-            font-weight: bold;
-            padding-right:30px;
-            padding-left: 30px;
-            color: white;
-            text-decoration:none;
-        }
-
-        .nav-navigasi ul li a:hover{
-            font-weight: bold;
-            padding: 30px;
-            color:#65C18C;
-        }
-
-        .btn{
-            color: #65C18C;
-            border-color: lightgray;
-        }
-
-        .btn:hover {
-            color: white;
-            background-color: #65C18C; 
-            border-color: #65C18C;
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body class="bg-light">
+
+<body>
     <nav class="nav">
         <div class="logo">CariSini UPNVJ</div>
         <div class="nav-navigasi">
@@ -108,10 +23,10 @@
         </div>
     </nav>
 
-    <div style="margin-top: 70px;">
-        <div class="container mt-5 border rounded bg-white py-4 px-5 mb-5">
-            <header style="margin-bottom:-30px;">
-                <h1 class="title"><a style="text-decoration: none;"><span style= "color:#186F65">Verifikasi </span><span>Pengajuan</span></a></h1>
+    <div class="flex-container">
+        <div class="container mt-3 border rounded bg-white py-4 px-5 mb-5">
+            <header>
+                <h1 class="title"><span style= "color:#186F65">Verifikasi </span><span>Pengajuan</span></h1>
                 <hr>
             </header>
 
@@ -123,26 +38,26 @@
                 ?>
 
                 <div class="table-responsive">
-                    <table class="table table-striped mt-4">
+                    <table class="table table-striped">
                         <thead>
-                            <tr>
-                                <th class="custom-th" scope="col">Nomor</th>
-                                <th class="custom-th" scope="col">NIM/NIP</th>
-                                <th class="custom-th" scope="col">Nama Lengkap</th>
-                                <th class="custom-th" scope="col">KTP/KTM</th>
-                                <th class="custom-th" scope="col">Kode Barang</th>
-                                <th class="custom-th" scope="col">Nama Barang</th>
-                                <th class="custom-th" scope="col">Tanggal Hilang</th>
-                                <th class="custom-th" scope="col">Spesifikasi</th>
-                                <th class="custom-th" scope="col">Kronologi Kehilangan</th>
-                                <th class="custom-th" scope="col">Status</th>
+                            <tr class="kolom">
+                                <th scope="col">Nomor</th>
+                                <th scope="col">NIM/NIP</th>
+                                <th scope="col">Nama Lengkap</th>
+                                <th scope="col">KTP/KTM</th>
+                                <th scope="col">Kode Barang</th>
+                                <th scope="col">Nama Barang</th>
+                                <th scope="col">Tanggal Hilang</th>
+                                <th scope="col">Spesifikasi</th>
+                                <th scope="col">Kronologi Kehilangan</th>
+                                <th scope="col">Status</th>
                             </tr>
                         </thead>
-                        <tbody>
+                        <tbody class="isi">
                             <?php
                             include("koneksi.php");
 
-                            $query = "SELECT * FROM pengajuan WHERE status = 'Belum Terverifikasi'";
+                            $query = "SELECT * FROM pengajuan";
                             $result = mysqli_query($koneksi, $query);
 
                             if(!$result){
@@ -191,6 +106,9 @@
             </section>
         </div>
     </div>
+    <footer>
+        All Rights Reserved | © CariSini UPNVJ! - 2024
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
