14b2ec0c0eb7155e10e600a0bdac9bb0ce8b5431
GiannSinaga <giannnagara@gmail.com>
2024-06-13 22:29:37 +0700

    login-terbaru
    Halaman login udah di design

--- login.php
+++ login.php
@@ -34,17 +34,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cari Sini UPNVJ </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;700&display=swap" rel="stylesheet">
     <style>
         body {
             display: flex;
             justify-content: center;
             align-items: center;
             height: 100vh;
+            background-color: #186F65;
+        }
+
+
+        h3 {
+            margin: 50px 50px 100px 50px;
+            text-align: center;
+            font-size: 100px;
+            color: white;
+            font-family: 'Poppins';
+            font-weight: bold;
         }
 
         input[type=text],
         input[type=password] {
-            width: 100%;
             padding: 12px 20px;
             margin: 8px 0;
             display: inline-block;
@@ -53,20 +65,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             box-sizing: border-box;
         }
 
-        h3 {
-            margin: 50px 50px 100px 50px;
-            text-align: center;
-            color: black;
-        }
-
-        label {
-            text-align: center;
-            color: black;
+        input {
+            width: 1000px;
+            align-items: center;
+            display: flex;
+            justify-content: center;
+            /* color: black; */
         }
 
         input[type=submit] {
-            width: 100%;
-            text-align: center;
             padding: 14px 20px;
             background-color: #65C18C;
             border: 1px solid grey;
@@ -80,19 +87,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             background-color: white;
             border: 1px solid #65C18C;
             border-radius: 30px;
-            color: #65C18C
+            color: #65C18C;
         }
 
         .container {
-            background: white;
             display: flex;
             justify-content: center;
-            align-items: center;
-            border: 1px solid grey;
-            border-radius: 30px;
-            padding: 10px 20px 50px 20px;
-            font-family: sans-serif;
-            box-shadow: 5px 5px 5px lightgrey;
         }
     </style>
 </head>
@@ -100,16 +100,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
     <div class="container">
         <form action="" method="post">
-            <h3>CARI SINI<br>UPNVJ</h3>
+            <h3>CariSini UPNVJ!</h3>
             <?php
             if (isset($error)) {
                 echo "<p style='color:red;'>$error</p>";
             }
             ?>
-            <label for="username"><b>Username</b></label>
+            <!-- <label for="username"><b>Username</b></label> -->
             <input type="text" placeholder="Masukkan username" name="username" required><br><br>
 
-            <label for="password"><b>Password</b></label>
+            <!-- <label for="password"><b>Password</b></label> -->
             <input type="password" placeholder="Masukkan password" name="password" required>
             <br><br>
 
